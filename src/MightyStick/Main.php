9d5a95af48862f59b8e827949b1c417047f73667
RetiredDev <36087326+RetiredDev@users.noreply.github.com>
2018-02-21 23:05:46 +0000

    Expecting many errors

--- src/MightyStick/Main.php
+++ src/MightyStick/Main.php
@@ -13,12 +13,14 @@ use pocketmine\entity\Effect;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
 
 
 class Main extends PluginBase implements Listener {
 	
 	public function onEnable() {
 		
+		$this->saveDefaultConfig();
 		$this->getServer()->getLogger()->notice("MightyStick has been enabled!");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		
@@ -28,9 +30,9 @@ class Main extends PluginBase implements Listener {
 		
 		if(strtolower($command->getName()) === "stick") {
 			
-			if(count($args) < 2) {
+			if(count($args) < 1) {
 			
-				$sender->sendMessage("§cPlease use: /stick <player> 1");
+				$sender->sendMessage("§cPlease use: /stick <player>");
 				return true;
 			 
 			}
@@ -39,15 +41,14 @@ class Main extends PluginBase implements Listener {
 				if(isset($args[0])) {
 				
 					$player = $sender->getServer()->getPlayer($args[0]);
-					
-					if(isset($args[1])) {
-							
-							$tier1 = Item::get(Item::LEVER, 5, 1);
-							$tier1->setCustomName(TF::RESET . TF::BOLD . TF::YELLOW . "Mighty Stick" . TF::RESET . TF::GRAY . " (Tap anywhere)" . PHP_EOL . PHP_EOL . 
+
+									
+							$stick = Item::get(Item::LEVER, 2, 1);
+							$stick->setCustomName(TF::RESET . TF::BOLD . TF::YELLOW . "Mighty Stick" . TF::RESET . TF::GRAY . " (Tap anywhere)" . PHP_EOL . PHP_EOL . 
 							TF::DARK_GRAY . " *" . TF::AQUA . " Chance to get: " . PHP_EOL .
 							TF::DARK_GRAY . " *" . TF::GRAY . "Effects");
 							
-							$player->getInventory()->addItem($tier1);
+							$player->getInventory()->addItem($stick);
 							
 							break;
 							
@@ -72,34 +73,85 @@ class Main extends PluginBase implements Listener {
 		return true;
 		
 	}
-			
+	
 	public function onInteract(PlayerInteractEvent $event) {
 		
 		$player = $event->getPlayer();
 		
 		if($event->getItem()->getId() === 69) {
 		
-		if($event->getItem()->getDamage() === 5) {
-
-				$tier1 = Item::get(Item::LEVER, 5, 1);
+		if($event->getItem()->getDamage() === 2) {
 				
-				$effect = Effect::getEffect("5");
-                                                    $effect->setDuration("1000000" * 18);
-                                                    $effect->setAmplifier("4");
-                                                    $player->addEffect($effect);
-                                                    $effectb = Effect::getEffect("2");
-                                                    $effectb->setDuration("1000000" * 18);
-                                                    $player->addEffect($effectb);
-                                                    $effectc = Effect::getEffect("11");
-                                                    $effectc->setDuration("1000000" * 18);
-                                                    $effectc->setAmplifier("4");
-                                                    $player->addEffect($effectc);
+				$random = (mt_rand(1, 5)
+				
+	switch($random) {
+				
+				case 1:
+				
+				$stick = Item::get(Item::LEVER, 2, 1);
+   
+				foreach($this->getConfig()->get("first-stick") as $effects) {
+				
+				$player->addEffect(Effect::get($effects, mt_rand($this->getConfig()->get("effect-min"), $this->getConfig()->get("effect-max"))));
 				
 				$player->addTitle("§aUsed §eMighty Stick");
-				$player->getInventory()->removeItem($tier1);
+				$player->getInventory()->removeItem($stick);
 				
 				break;
 				
+				case 2:
+				
+				$stick = Item::get(Item::LEVER, 2, 1);
+   
+				foreach($this->getConfig()->get("second-stick") as $effects) {
+				
+				$player->addEffect(Effect::get($effects, mt_rand($this->getConfig()->get("effect-min"), $this->getConfig()->get("effect-max"))));
+				
+				$player->addTitle("§aUsed §eMighty Stick");
+				$player->getInventory()->removeItem($stick);
+				
+				break;
+				
+				case 3:
+				
+				$stick = Item::get(Item::LEVER, 2, 1);
+   
+				foreach($this->getConfig()->get("third-stick") as $effects) {
+				
+				$player->addEffect(Effect::get($effects, mt_rand($this->getConfig()->get("effect-min"), $this->getConfig()->get("effect-max"))));
+				
+				$player->addTitle("§aUsed §eMighty Stick");
+				$player->getInventory()->removeItem($stick);
+				
+				break;
+				
+				case 1:
+				
+				$stick = Item::get(Item::LEVER, 2, 1);
+   
+				foreach($this->getConfig()->get("fourth-stick") as $effects) {
+				
+				$player->addEffect(Effect::get($effects, mt_rand($this->getConfig()->get("effect-min"), $this->getConfig()->get("effect-max"))));
+				
+				$player->addTitle("§aUsed §eMighty Stick");
+				$player->getInventory()->removeItem($stick);
+				
+				break;
+				
+				case 1:
+				
+				$stick = Item::get(Item::LEVER, 2, 1);
+   
+				foreach($this->getConfig()->get("fifth-stick") as $effects) {
+				
+				$player->addEffect(Effect::get($effects, mt_rand($this->getConfig()->get("effect-min"), $this->getConfig()->get("effect-max"))));
+				
+				$player->addTitle("§aUsed §eMighty Stick");
+				$player->getInventory()->removeItem($stick);
+				
+				break;
+				
+			}
 		}
 	}
 	
@@ -109,7 +161,7 @@ class Main extends PluginBase implements Listener {
 			
 			$damage = $event->getItem()->getDamage();
 			
-			if($damage === 5) {
+			if($damage === 2) {
 				
 				$event->setCancelled();
 				
